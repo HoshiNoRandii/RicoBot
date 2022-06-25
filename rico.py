0929e3db66f3ee08edd65b17944c89816161c046
HoshiNoRandii <108156355+HoshiNoRandii@users.noreply.github.com>
2022-06-24 20:19:07 -0400

    Update rico.py

--- rico.py
+++ rico.py
@@ -23,11 +23,11 @@ async def on_ready():
 @client.event
 async def on_message(message):
     # don't have Rico reply to himself
-    if message.auther == client.user:
+    if message.author == client.user:
         return
 
     # respond to messages starting with 'r!'
     if message.content.startswith('r!'):
-        await message.channel.send('henlo!'):
+        await message.channel.send('henlo!')
 
 client.run(token)
