689cbd2395a24f65a43a8e8e4d721c1925969d29
HoshiNoRandii <108156355+HoshiNoRandii@users.noreply.github.com>
2022-06-24 22:15:58 -0400

    can grab a nickname

--- rico.py
+++ rico.py
@@ -20,32 +20,55 @@ client = discord.Client(intents=intents)
 async def on_ready():
     print('Rico is ready!')
 
+
 @client.event
 async def on_message(message):
     # don't have Rico reply to himself
     if message.author == client.user:
         return
 
+
     # respond to messages starting with 'r!'
     if message.content.startswith('r!'):
-
         # split the messgage into individual words
         splitMessage = message.content.split()
 
-        # if no command, respond with '?'
+
+        # no command: send '?'
         if len(splitMessage) < 2:
             await message.channel.send('?')
             return
 
+
         # grab the command
         command = splitMessage[1]
 
-        # respond 'henlo!' to the command 'hi'
+
+        # nick: change the nickname of the mentioned people
+        # syntax: r! nick @user1 @user2 ... [desired nickname]
+        # NOTE: currently can only grab nickname from a command with
+        # correct syntax
+        if command == 'nick':
+            # grab the nickname
+            # find where in the message the nickname starts
+            nickIndex = 2
+            while(splitMessage[nickIndex].startswith('<@')):
+                nickIndex +=1
+            # slice off the nickname
+            nickList = splitMessage[nickIndex:]
+            # make it one string again
+            nick = ' '.join(nickList)
+            print(nick)
+            return
+
+
+        # hi: send 'henlo!'
         if command == 'hi':
             await message.channel.send('henlo!')
             return
 
-        # respond '?' to anything else
+
+        # other command: send '?'
         else:
             await message.channel.send('?')
             return
