07759eeadf073cdcf013ac82d5beb153670c46a9
HoshiNoRandii <108156355+HoshiNoRandii@users.noreply.github.com>
2023-10-18 16:18:06 -0400

    emacs didn't like my single quotes

--- rico.py
+++ rico.py
@@ -5,7 +5,7 @@
 #
 # I wrote this bot so that my friends could all change each other's
 # nicknames in our server!
-# 
+#
 ###############################################################################
 
 import asyncio
@@ -27,34 +27,36 @@ BETA = True
 # loading in the token
 load_dotenv()
 if BETA == True:
-    token = environ['BETA_TOKEN']
+    token = environ["BETA_TOKEN"]
 else:
-    token = environ['TOKEN']
+    token = environ["TOKEN"]
 
 # making sure to have all necessary permissions
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 
-bot = commands.Bot(intents = intents, command_prefix = ('r! ','r!','R! ','R!'))
+bot = commands.Bot(intents=intents, command_prefix=("r! ", "r!", "R! ", "R!"))
 # remove default help command so the custom one works
-bot.remove_command('help')
+bot.remove_command("help")
 
 # create the connection pool for the postgres server
 connect.init()
 
+
 # load in the cogs
 async def loadCogs():
-    cogsList = ['commands', 'dev_tools']
+    cogsList = ["commands", "dev_tools"]
     for cog in cogsList:
         await bot.load_extension(cog)
+        print(f"{cog} cog loaded")
 
 
 @bot.event
 async def on_ready():
-    playing = discord.Game(name = 'r! help')
-    await bot.change_presence(activity = playing)
-    print('Rico is ready!')
+    playing = discord.Game(name="r! help")
+    await bot.change_presence(activity=playing)
+    print("Rico is ready!")
 
 
 @bot.event
@@ -62,15 +64,17 @@ async def on_message(message):
     # don't have Rico reply to himself
     if message.author == bot.user:
         return
-    
+
     # so that commands work as well as anything in here
     await bot.process_commands(message)
-    
+
     return
+
 
 async def main():
     async with bot:
         await loadCogs()
         await bot.start(token)
+
 
 asyncio.run(main())
