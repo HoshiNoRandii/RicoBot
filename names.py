14fb08fe1841eadb0514c4249f0eea0df1cd6ccb
HoshiNoRandii <108156355+HoshiNoRandii@users.noreply.github.com>
2023-11-01 12:44:41 -0400

    have isNameRole search db

--- names.py
+++ names.py
@@ -69,19 +69,25 @@ Notes:
         aliases=["setName", "Setname", "SetName"],
         brief="set your name",
         help="""Type `r! setname [name]` to change your name to [name]. This will be the name people see when they use the `getname` command, and will also be listed as a role.\n
-Character Limit: 29""",
+Character Limit: 32""",
     )
     @db_connector_with_args
     async def setName(self, ctx, *args, cursor):
         newName = " ".join(args)
         # check that name isn't too long
-        if len(newName) > 29:
-            await ctx.channel.send("that name is too long! (max: 29 characters)")
+        if len(newName) > 32:
+            await ctx.channel.send("that name is too long! (max: 32 characters)")
             return
-        updateNameDB(ctx.guild, ctx.author, newName, cursor)
-        await updateNameRole(ctx.guild, ctx.author, cursor)
+        oldName = updateNameDB(ctx.guild, ctx.author, newName, cursor)[0]
+        await updateNameRole(ctx.guild, ctx.author, cursor, oldName)
         await ctx.channel.send("name set!")
         return
+
+    ## TODO:
+    ## make sure to update the db if somebody
+    ## makes manual changes to their name
+    # @commands.Cog.listener("on_member_update")
+    # async def manualUpdate(self, before, after):
 
 
 ## helper functions ##
@@ -90,25 +96,38 @@ Character Limit: 29""",
 # updates a user's name in the user_list table
 # server argument is an instance of discord.Guild
 # member argument is an instance of discord.Member
+# returns (oldName, newName)
 def updateNameDB(server, member, newName, cursor):
     serverID = server.id
     userID = member.id
     tableName = f"user_list_{serverID}"
+
+    # get old name
+    getOldName = f"""
+    SELECT name
+    FROM {tableName}
+    WHERE user_id = {userID}
+    """
+    cursor.execute(getOldName)
+    oldName = cursor.fetchall()[0][0]
+
+    # set new name
     updateName = f"""
     UPDATE {tableName}
     SET name = '{newName}'
     WHERE user_id = {userID}
     """
     cursor.execute(updateName)
-    print(f"user {member.name} name set to {newName} in user_list table")
+    print(f"user {member.name}'s name set to {newName} in user_list table")
 
-    return
+    return (oldName, newName)
 
 
 # updates a user's name role
 # server argument is an instance of discord.Guild
 # member argument is an instance of discord.Member
-async def updateNameRole(server, member, cursor):
+# oldName argument is optional
+async def updateNameRole(server, member, cursor, oldName=None):
     # grab name from database
     serverID = server.id
     tableName = f"user_list_{serverID}"
@@ -121,25 +140,55 @@ async def updateNameRole(server, member, cursor):
     cursor.execute(getName)
     # cursor.fetchall() returns a list of tuples, where each tuple
     # is a returned row
-    # this cursor.fetchall() should return [(name)]
+    # this cursor.fetchall() should return [(name,)]
     newName = cursor.fetchall()[0][0]
 
     # check for existing name role
     for role in member.roles:
-        if isNameRole(role):
-            await role.edit(name=f"n: {newName}")
+        if isNameRole(server, role, cursor, oldName):
+            await role.edit(name=f"{newName}")
             return
 
     # if no existing name role, create one
-    role = await server.create_role(name=f"n: {newName}")
+    role = await server.create_role(name=f"{newName}")
     await member.add_roles(role)
     return
 
 
 # isNameRole checks if a role is a "name role",
-# i.e., if it starts with "n: "
-def isNameRole(role):
-    return role.name.startswith("n: ")
+# i.e. its name matches a name in the user_list table
+# or the user's previous name, if the optional
+# oldName argument is provided
+def isNameRole(server, role, cursor, oldName=None):
+    # grab list of names from db
+    serverID = server.id
+    tableName = f"user_list_{serverID}"
+    getNames = f"""
+    SELECT name
+    FROM {tableName}
+    """
+    cursor.execute(getNames)
+    # cursor.fetchall() returns a list of tuples, where each tuple
+    # is a returned row
+    # this cursor.fetchall() should return
+    # [(name1,),(name2,),(name3,),...]
+    nameList = listUntuple(cursor.fetchall())
+    if oldName:
+        nameList.append(oldName)
+    print(f"isNameRole {role.name}: {role.name in nameList}")
+    print(f"nameList: {nameList}")
+    return role.name in nameList
+
+
+# listUntuple takes a list of tuples and "unpacks" all the tuples,
+# returning a list
+# ex: listUntuple([(1,), (2,3), (4,)]) == [1, 2, 3, 4]
+def listUntuple(tupList):
+    retList = []
+    for tup in tupList:
+        for x in tup:
+            retList.append(x)
+    return retList
 
 
 # necessary to link the cog to the main file
