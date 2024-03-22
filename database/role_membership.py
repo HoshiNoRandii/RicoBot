6c8e98b52e908e97df192aadc4074708da0057e4
HoshiNoRandii <108156355+HoshiNoRandii@users.noreply.github.com>
2024-03-21 23:24:55 -0400

    add rmUpdate, rmGetUserIDs

--- database/role_membership.py
+++ database/role_membership.py
@@ -3,6 +3,9 @@
 ### are assigned to which of the roles managed by RicoBot
 
 # columns in the role_membership table
+from database.bot_roles import isBotRole
+
+
 roleMembershipColumns = ["user_id", "role_id"]
 
 
@@ -36,3 +39,70 @@ def rmCreate(server, cursor):
         print(error)
 
     return
+
+
+def rmUpdate(user, role, cursor):
+    """
+    Add a row to the role_membership table
+
+    args:
+        user: discord.Member
+        role: discord.Role
+        cursor: psycopg2.cursor
+
+    returns: None
+    """
+    try:
+        # check if role is managed by RicoBot
+        if not isBotRole(role, cursor):
+            return
+        # role is managed by RicoBot, add row to table
+        serverID = role.guild.id
+        tableName = f"role_membership_{serverID}"
+        userID = user.id
+        roleID = role.id
+        insertInto = f"""
+            INSERT INTO {tableName} (user_id, role_id)
+            VALUES ({userID}, {roleID})
+        """
+        cursor.execute(insertInto)
+        print(
+            f"{user.name} registered as a member of {role.name} role in role_membership table"
+        )
+
+    except Exception as error:
+        print(error)
+
+    return
+
+
+def rmGetUserIDs(role, cursor):
+    """
+    Generator function that yields user ID's of
+    members of the given role.
+
+    args:
+        role: discord.Role
+        cursor: psycopg2.cursor
+
+    yields: int
+    """
+    try:
+        serverID = role.guild.id
+        tableName = f"role_membership_{serverID}"
+        roleID = role.id
+        select = f"""
+            SELECT user_id FROM {tableName}
+            WHERE role_id = {roleID}
+        """
+        cursor.execute(select)
+        # cursor.fetchall() returns a list of tuples,
+        # where each tuple is a returned row
+        # this cursor.fetchall returns [(id,)]
+        members = cursor.fetchall()
+
+        for (member,) in members:
+            yield member
+
+    except Exception as error:
+        print(error)
